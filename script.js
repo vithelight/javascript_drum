e73b22d7bf61c16fbd3660a730692a225ba88247
Umidjon Juraboev <vithelightning@gmail.com>
2024-02-05 14:31:17 +0500

    1st commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function playSound(e){
+  const audio = this.document.querySelector(`audio[data-key='${e.keyCode}']`);
+  const key = this.document.querySelector(`.key[data-key='${e.keyCode}']`);
+  if (!audio) return; // stops the function from running all together
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove('playing');
+}
+const keys = this.document.querySelectorAll(".key");
+keys.forEach((key) =>
+key.addEventListener("transitionend", removeTransition)
+);
+
+window.addEventListener("keydown", playSound);
